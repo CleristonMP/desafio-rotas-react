275aaed983188b714d26f0c41668419eaa23e883
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2023-03-13 20:28:55 -0300

    Products

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import About from "./routes/About";
 import Home from "./routes/Home";
 import HomeBody from "./routes/Home/HomeBody";
 import NotFound from "./routes/NotFound";
+import Products from "./routes/Products";
+import Books from "./routes/Products/Books";
+import Computers from "./routes/Products/Computers";
+import Electronics from "./routes/Products/Electronics";
 
 function App() {
   return (
@@ -11,6 +15,11 @@ function App() {
         <Route path="/" element={<Home />}>
           <Route index element={<Navigate to="/home" />} />
           <Route path="home" element={<HomeBody />} />
+          <Route path="products" element={<Products />}>
+            <Route path="computers" element={<Computers />} />
+            <Route path="electronics" element={<Electronics />} />
+            <Route path="books" element={<Books />} />
+          </Route>
           <Route path="about" element={<About />} />
           <Route path="*" element={<NotFound />} />
         </Route>

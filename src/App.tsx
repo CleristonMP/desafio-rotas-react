f71c19f52a3c1e23b2fc459aba96fa6e0da4851c
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2023-03-13 19:59:28 -0300

    Not found

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import About from "./routes/About";
 import Home from "./routes/Home";
 import HomeBody from "./routes/Home/HomeBody";
+import NotFound from "./routes/NotFound";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route index element={<Navigate to="/home" />} />
           <Route path="home" element={<HomeBody />} />
           <Route path="about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>

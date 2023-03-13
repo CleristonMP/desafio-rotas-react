5c61ab624c707900894d54619c4206d1dd10a31e
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2023-03-13 19:50:56 -0300

    Project created and clean, Home

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import HomeBody from "./routes/Home/HomeBody";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route index element={<Navigate to="/home" />} />
+          <Route path="home" element={<HomeBody />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
